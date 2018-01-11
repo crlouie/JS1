7d30515e6725f27515e372b469d1d9b95c06991c
crlouie <chris.louie@icloud.com>
2018-01-11 13:46:05 -0700

    Second Commit

--- apps.js
+++ apps.js
@@ -1,1 +1,15 @@
-alert("System Connected");
+// alert("System Connected");
+
+let A = "Bob";
+let B = "Smith";
+let C = ["Blue", "Bird"];
+
+
+function writeName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+
+console.log(writeName(A, B));
+console.log(writeName("Fred", "Dexter"));
+console.log(writeName(C[0], C[1]));
